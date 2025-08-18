b7dcc052bb28344c91274e7f38c575e0ced8507c
Devtech011 <devtech011@gmail.com>
2025-08-18 13:26:51 +0530

    GREEN: Implement custom delimiter parsing

--- src/utils/calculator.ts
+++ src/utils/calculator.ts
@@ -3,8 +3,18 @@ export function add(numbers: string): number {
         return 0
     }
 
-    const numberArray = numbers.split(/[,\n]/)
+    let delimiter = ','
+    let numbersString = numbers
+
+    if (numbers.startsWith('//')) {
+        const delimiterEndIndex = numbers.indexOf('\n')
+        delimiter = numbers.substring(2, delimiterEndIndex)
+        numbersString = numbers.substring(delimiterEndIndex + 1)
+    }
+
+    const numberArray = numbersString.split(new RegExp(`[${delimiter},\n]`))
     return numberArray
+        .filter(num => num.trim() !== '')
         .map(num => parseInt(num))
         .reduce((sum, num) => sum + num, 0)
 }
