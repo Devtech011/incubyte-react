fcc5218338f0076af18d186d3f31ca56393ab8bb
Devtech011 <devtech011@gmail.com>
2025-08-18 13:23:58 +0530

    RED: Add test for multiple numbers (already passes)

--- src/utils/calculator.test.ts
+++ src/utils/calculator.test.ts
@@ -15,4 +15,9 @@ describe('String Calculator', () => {
         expect(add('1,2')).toBe(3)
         expect(add('1,5')).toBe(6)
     })
+
+    it('should handle any amount of numbers', () => {
+        expect(add('1,2,3')).toBe(6)
+        expect(add('1,2,3,4,5')).toBe(15)
+    })
 })
