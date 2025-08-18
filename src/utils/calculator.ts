6b3d329c707457655fb8f8ff6149f233f3499555
Devtech011 <devtech011@gmail.com>
2025-08-18 13:58:02 +0530

    REFACTOR: Extract parseCustomDelimiter helper function

--- src/utils/calculator.ts
+++ src/utils/calculator.ts
@@ -3,18 +3,23 @@ export function add(numbers: string): number {
         return 0
     }
 
-    let delimiter = ','
-    let numbersString = numbers
+    const { delimiter, numbersString } = parseCustomDelimiter(numbers)
 
-    if (numbers.startsWith('//')) {
-        const delimiterEndIndex = numbers.indexOf('\n')
-        delimiter = numbers.substring(2, delimiterEndIndex)
-        numbersString = numbers.substring(delimiterEndIndex + 1)
-    }
-
-    const numberArray = numbersString.split(new RegExp(`[${delimiter},\n]`))
-    return numberArray
+    const numberArray = numbersString
+        .split(new RegExp(`[${delimiter},\n]`))
         .filter(num => num.trim() !== '')
         .map(num => parseInt(num))
-        .reduce((sum, num) => sum + num, 0)
+
+    return numberArray.reduce((sum, num) => sum + num, 0)
+}
+
+const parseCustomDelimiter = (numbers: string): { delimiter: string; numbersString: string } => {
+    if (numbers.startsWith('//')) {
+        const delimiterEndIndex = numbers.indexOf('\n')
+        const delimiter = numbers.substring(2, delimiterEndIndex)
+        const numbersString = numbers.substring(delimiterEndIndex + 1)
+        return { delimiter, numbersString }
+    }
+
+    return { delimiter: ',', numbersString: numbers }
 }
